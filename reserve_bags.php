8c3ab12b44fbe8ceca9cbe32bf855bd02710fd97
Cayden Codel <ccodel@andrew.cmu.edu>
2020-08-24 11:04:11 -0400

    Updated email form

--- reserve_bags.php
+++ reserve_bags.php
@@ -4,9 +4,9 @@ $email = $_POST["email"];
 $date = $_POST["date"];
 $time = $_POST["time"];
 
-$msg = $name . " would like to reserve the bags court on " . $date . " at " . $time;
+$msg = $name . " would like to reserve the bags court on " . $date . " at " . $time . " for 30 minutes. If you would like to reply to " . $name . ", please use the following email: " . $email;
 
-mail("crcodel@gmail.com", "Bags reservation for " . $name, $msg);
+mail($email, "Bags reservation for " . $name, $msg);
 
 header("Location: index.html");
 exit();
